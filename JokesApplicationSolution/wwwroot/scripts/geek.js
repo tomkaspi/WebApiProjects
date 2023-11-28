2093e46338673233e58bbfdcdc1000d8e175efc9
tomkaspi <tomkaspi@yahoo.com>
2023-11-28 09:27:08 +0100

    valtozas

---
+++ JokesApplicationSolution/wwwroot/scripts/geek.js
@@ -0,0 +1,20 @@
+﻿var viccek;
+
+function letoltes() {
+    console.log("this runs");
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+}
+
+window.onload = function () {
+    console.log('window - onload');
+    letoltes();
+};
